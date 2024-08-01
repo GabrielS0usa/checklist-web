80ba067801df8c69157d5455ecece74260dd1740
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2024-08-01 13:04:43 -0300

    Adding sidenav

--- checklist-front-end/src/app/app.component.ts
+++ checklist-front-end/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {CommonModule} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MatSidenavModule, MatCheckboxModule, FormsModule,MatButtonModule,CommonModule,MatListModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'checklist-front-end';
+
+  public events: string[] = [];
+  public opened: boolean = false;
+
 }
--- checklist-front-end/src/app/header/header.component.ts
+++ checklist-front-end/src/app/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +12,14 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() sidenav!: MatSidenav
+
   ngOnInit(): void {
   }
 
   public openSideNav(){
-
+    this.sidenav.toggle()
   }
 
 }
